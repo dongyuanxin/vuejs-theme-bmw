3fc4a680f235a351d8dfe5ee71f9d6dc9c4c1f5a
dongyuanxin <yuanxin.me@gmail.com>
2018-08-21 16:30:03 +0800

    优化: 根据 process.env.NODE_ENV 动态设置setting

--- src/vendor/setting.js
+++ src/vendor/setting.js
@@ -1,10 +1,14 @@
 const cdn = {
   bgImgUrl: "/image/wallpaper", // 背景图片地址
-  mdImgUrl: "http://blog-1255463368.file.myqcloud.com/markdown-static" // markdown图片地址
+  mdImgUrl:
+    process.env.NODE_ENV === "development"
+      ? "https://raw.githubusercontent.com/dongyuanxin/markdown-static/master/"
+      : "http://blog-1255463368.file.myqcloud.com/markdown-static" // markdown图片地址
 };
 
 const server = {
-  url: "//193.112.241.232"
+  url:
+    process.env.NODE_ENV === "development" ? "//localhost" : "//193.112.241.232"
 };
 
 const lc = {
