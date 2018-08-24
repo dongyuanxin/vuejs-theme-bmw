c5ebf40f0a25b5c0199cf612951e0104ef0c71e3
dongyuanxin <yuanxin.me@gmail.com>
2018-08-24 18:08:48 +0800

    优化: 优化SEO

--- src/vendor/router.js
+++ src/vendor/router.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import Passage from "@/vendor/passage";
+
+const psgAPI = new Passage();
+
 Vue.use(Router);
 
 const router = new Router({
@@ -9,7 +13,10 @@ const router = new Router({
     {
       path: "/",
       name: "home",
-      component: () => import("@/views/Home")
+      component: () => import("@/views/Home"),
+      meta: {
+        title: "董沅鑫的个人网站"
+      }
     },
     {
       path: "/blog/:page",
@@ -29,22 +36,31 @@ const router = new Router({
     {
       path: "/friend",
       name: "friend",
-      component: () => import("@/views/Friend")
+      component: () => import("@/views/Friend"),
+      meta: {
+        title: "友链 - GODBMW"
+      }
     },
     {
       path: "/about",
       name: "about",
-      component: () => import("@/views/About")
+      component: () => import("@/views/About"),
+      meta: {
+        title: "关于 - GODBMW"
+      }
     },
     {
       path: "/archive",
       name: "archive",
-      component: () => import("@/views/Archive")
+      component: () => import("@/views/Archive"),
+      meta: {
+        title: "分类与标签 - GODBMW"
+      }
     },
     {
       path: "*",
       name: "error",
-      component: () => import("@/views/Home")
+      redirect: "/"
     }
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -53,6 +69,45 @@ const router = new Router({
     } else {
       return { x: 0, y: 0 };
     }
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+    return next();
+  }
+  if (to.name === "search") {
+    if (to.query.hasOwnProperty("category")) {
+      document.title = "分类:" + to.query.category + " - GODBMW";
+    } else if (to.query.hasOwnProperty("tag")) {
+      document.title = "标签:" + to.query.tag + " - GODBMW";
+    } else if (to.query.hasOwnProperty("time")) {
+      document.title = "发布时间:" + to.query.time + " - GODBMW";
+    } else {
+      return next({
+        name: "home"
+      });
+    }
+    return next();
+  } else if (to.name === "blog") {
+    document.title = "第" + to.params.page + "页 - GODBMW";
+    return next();
+  } else if (to.name === "passage") {
+    psgAPI
+      .search(to.params.id)
+      .then(res => {
+        if (res.hasOwnProperty("title") && res.title.length > 0) {
+          document.title = res.title + " - GODBMW";
+          return next();
+        }
+        return next({ name: "home" });
+      })
+      .catch(err => {
+        return next({ name: "home" });
+      });
+  } else {
+    return next();
   }
 });
 
